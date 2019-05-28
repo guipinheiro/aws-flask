3a40949d2d120208bdc086f3a860e32aaa5fa4a6
Guilherme Pinheiro <guilhermepinheiro@Guilhermes-MacBook-Air.local>
2019-05-27 22:44:45 -0300

    updating config file after db configuration

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+from amazon_aws_data import *
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_password}@{db_end}:{db_port}/{db_server}'
+
+#mysql+pymysql://guipinheiro:22mrcgrl:P@{db_end}:{db_port}/{db_server}'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
